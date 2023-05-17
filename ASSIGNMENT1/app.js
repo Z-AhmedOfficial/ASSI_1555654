0f8c67b644d6063c4980cbd1f43cda1e81a0704f
Z-AhmedOfficial <128061591+Z-AhmedOfficial@users.noreply.github.com>
2023-05-17 21:15:34 +0500

    Add files via upload

---
+++ ASSIGNMENT1/app.js
@@ -0,0 +1,200 @@
+// CHAPTER 1://
+// alert("FirstNAME" + " Zain");
+// alert("LasttNAME" + " Ahmed");
+// alert("Email" + " fredthered900@gmail.com");
+// alert("Number" + " 0335-2861383");
+// alert("Password" + "  123Abcde");
+// Correct this statement: alert"You're learning JavaScript!";
+// alert("You're Learning Javascript");
+// Code an alert statement displaying any message you like.
+// alert("Welcome to this Page");
+
+//CHAPTER 2://
+// var someVar;
+// var str;
+// var str = "Hello there";
+// alert(str);
+// var TeamName = " Manchester United"
+// alert("TeamName is" + TeamName);
+// var bestMan = "Charlie";
+// var bestMan = "Jhon";
+// alert(bestMan)
+
+//CHAPTER 3://
+// var caseQty = 144;
+// var num = +"9";
+// var sum;
+// var sum = caseQty + num;
+// alert(sum);
+// var merchTotal = 100;
+// var shippingCharge = 10;
+// var orderTotal = merchTotal + shippingCharge;
+// alert(orderTotal); //ANSWER IS 110;
+// var a = 15;
+// var b = a + 29;
+// alert(b);
+
+//CHAPTER 4://
+// var productcost = 3.45;
+// var Nameofband = "SomeBandName";
+// var camelCaseName = Nameofband.charAt(0).toLowerCase() + Nameofband.slice(1);
+// console.log("Camel case name:", camelCaseName);
+// var legalvar2 = 45;
+// console.log(legalvar2);
+// var fname = "zain"
+// var Lstname = "  ahmed"
+// var combname = fname + Lstname;
+// console.log(combname);
+
+// Examples of legal variable names:
+// var myVariable;
+// var _privateVar;
+// var myVar2;
+// var firstName;
+
+// Examples of illegal variable names:
+// var 123abc;
+// var my variable;
+// var if;
+
+//CHAPTER 5://
+// What is the name and symbol of the arithmetic operator that
+// gives you the remainder when one number is divided by another?
+// ANSWER IS MODULO OPERATOR. (abbreviated as mod).
+// What is the value of num?
+// var num = 20 % 6;
+// console.log(num); //ANSWER IS 2;
+// var largeNum = 1000 * 2000;
+// console.log(largeNum);
+// var a = 4;
+// var b = a;
+// var c = 2;
+// var d = c;
+// var res = b - d;
+// console.log(res);
+// const dividend = 10;
+// const divisor = 3;
+// const remainder = dividend % divisor;
+// console.log(remainder);
+// var a = 3;
+// var b = 12;
+// var c = a * b;
+// alert(c);
+
+
+//CHAPTER 6://
+// q1
+// var x = 5;
+// x += 1;
+// console.log("New value of x:", x);
+// q2
+// var x = 100;
+// x -= 1;
+// console.log("New value of x:", x);
+// q4
+// var y = 50;
+// var z = --y;
+// console.log("Value of y:", y);
+// console.log("Value of z:", z);
+// q5
+// var num = 10;
+// var newNum = num--;
+// console.log("Value of num:", num);
+// console.log("Value of newNum:", newNum);
+// q6
+// var num = 10;
+// var newNum = num++;
+// console.log("Value of num:", num);
+// console.log("Value of newNum:", newNum);
+// q7
+// var num = 5;
+// num++;
+// alert("New value: " + num);
+
+
+//CHAPTER 7://
+// q1
+// var calculatedNum = 2 + (2 * 6);
+// console.log(calculatedNum)
+// q2
+// var calculatedNum = (2 + 2) * 6;
+// console.log(calculatedNum)
+// q3
+// var calculatedNum = (2 + 2) * (4 + 2);
+// console.log(calculatedNum)
+// q4
+// var calculatedNum = ((2 + 2) * 4) + 2;
+// console.log(calculatedNum)
+// q5
+// var cost = (2 + (2 * 4) + 10);
+// console.log(cost);
+// q6
+// var units = ((2 + 2) * 4) + 10;
+// console.log("Units:", units);
+// q7
+// var pressure = (4 / 2) * 4;
+// console.log(pressure);
+
+//CHAPTER 8://
+// q1
+// var num = "2" + "2";
+// What is the value of num? Include quotation marks.
+// q2
+// var message = ("Hello," + "Dolly");
+// alert(message);
+// q3
+// alert("33" + 3);
+// q4
+// var PAK = "Pakistan";
+// var ZIN = "Zindabad";
+// alert(PAK + ZIN);
+// q5
+// var hlo = "Hello" + 123;
+// q6
+// var a = "ali"
+// var b = " ahmed"
+// var result = console.log(a + b);
+
+
+//CHAPTER 9://
+// var firstName = prompt("Enter First Name");
+// var Country = prompt("Counrty?", "China")
+// Correct this statement
+// var yourName = prompt(Enter Your Name");
+// var yourName = prompt("Enter Your Name");
+// var str = prompt("Welcome", "Write something");
+// var a = "Vini";
+// var b = "jr";
+// var c = prompt(a, b);
+// var d = prompt("Enter a string");
+// var e = d;
+// console.log(e);
+// var message = prompt("How are you?", "Good");
+// alert(message);
+
+//CHAPTER: 10//
+// Q1:
+// var city = "Karachi"
+// if (city = "Karachi") {
+//   console.log("The City OF Lights")
+// }
+//Q2:
+// var x = 8;
+// var y = 8;
+// if (x === y) {
+//   var z = prompt("Enter the value of Z? ")
+// }
+// Q3:
+// var ZipCode = 10010;
+// if (ZipCode == 10010) {
+//   alert("Karachi")
+// }
+// else{
+//   alert("Please write correct city");
+// }
+// Q4:
+// var x = 4;
+// if (x == 4) {
+//   var x = 10;
+//   alert(x);
+// }
